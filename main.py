519d0b0bd7818d55da56e124773218469815e295
Shaik Tabrez <37013844+tabrezdn1@users.noreply.github.com>
2024-03-28 16:37:23 -0400

    Add docker file

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ DATA_URL = os.getenv('QR_DATA_URL', 'https://github.com/tabrezdn1')
 # Directory for saving QR code
 QR_DIRECTORY = os.getenv('QR_CODE_DIR', 'qr_code')
 # Filename for the QR code
-QR_FILENAME = os.getenv('QR_CODE_FILENAME', 'MyQRCode2.png')
+QR_FILENAME = os.getenv('QR_CODE_FILENAME', 'MyGitHub.png')
  # Fill color for the QR code
 FILL_COLOR = os.getenv('FILL_COLOR', 'black')
  # Background color for the QR code
